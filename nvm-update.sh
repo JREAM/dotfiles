781a87dbcae5dc8453e3a4dfd9eb5aa30abeeba3
Jesse Boyer <imboyus@gmail.com>
2017-11-08 16:40:03 -0500

    nvm update

--- nvm-update.sh
+++ nvm-update.sh
@@ -73,6 +73,7 @@ PACKAGES=(
   stylelint-scss
   standard
   tslint
+  tsutils
   typescript
   protractor
   karma-cli
@@ -112,39 +113,42 @@ VERSION=$(node --version)
 
 # Friendly Output Reminder
 echo -e "--> ${YLW}Your current active Node is set to: ${GRN} ${VERSION} ${NC}"
-echo -e "---------------------------------------------------------------------"
+echo -e "══════════════════════════════════════════════════════════════════════"
 echo -e "--> To Upgrade and/or Check Version:"
 echo -e "--> [?] If you haven't updaed, do the following:"
 echo -e "    ..   $ nvm ls-remote --> to see the latest version."
 echo -e "    ..   $ nvm install v8.X.X"
 echo -e "    ..   $ nvm alias default v8.X.X  --> sets the deafult version"
 echo -e "    ..   (!) ENSURE (!) Open a new terminal and type: $ node --version  -->  to ensure it's set correctly"
-echo -e "---------------------------------------------------------------------"
+echo -e ""
 
-echo -e "${GRN}---------------------------------------------------------------------"
+echo -e "${GRN}══════════════════════════════════════════════════════════════════════"
 echo -e "Packages To Install Globally:"
-echo -e "---------------------------------------------------------------------${NC}"
+echo -e "══════════════════════════════════════════════════════════════════════${NC}"
 
 # Easier to read output
-DISPLAY_OUTPUT=$( IFS=$'\n'; echo "${PACKAGES[*]}" )
+for value in "${PACKAGES[@]}"; do
+  printf "${YLW}%-8s${NC}\n" "${value}"
+done | column
+
+#DISPLAY_OUTPUT=$( IFS=$'\n'; echo "${PACKAGES[*]}" )
 echo -e "${YLW}${DISPLAY_OUTPUT} ${NC}"
-echo -e "---------------------------------------------------------------------${NC}"
+echo -e "${GRN}══════════════════════════════════════════════════════════════════════${NC}"
 
 # Prompt Before Installation
-read -ep "--> Install all of the dependencies for this Node version? [y/N]: " yn
+read -ep "--> Install all of the dependencies for this Node version? [ y/N ]: " yn
 
 if [[ $yn =~ ^[yY]$ ]]; then
 
-  echo -e "---------------------------------------------------------------------${NC}"
+  echo -e "${GRN}══════════════════════════════════════════════════════════════════════${NC}"
   echo -e "${GRN}Preparing to install, you may CTRL+C to cancel${NC}"
-  echo -e "---------------------------------------------------------------------${NC}"
+  echo -e "${GRN}══════════════════════════════════════════════════════════════════════${NC}"
 
 
   # Brief Timeout incase mind is changed, regardless this is all no big deal.
-  TIMEOUT=10
+  TIMEOUT=7
   while [ $TIMEOUT -gt 0 ]; do
     echo -e "${YLW}Running in ${TIMEOUT} seconds ... ( CTRL+C to cancel )${NC}"
-
     sleep 1
     : $((TIMEOUT--))
   done
