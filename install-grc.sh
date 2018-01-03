ad7ba5845352d9394aef8fa7c214bbdfbb0cd9de
Jesse Boyer <imboyus@gmail.com>
2018-01-03 04:31:55 -0500

    Remove swagger codegen, its in dockerrc if needed, easier. dockerrc check if docker installed, no more warning.

--- install-grc.sh
+++ install-grc.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+echo "[+] Installing grc"
 sudo apt install grc
 
+echo "[+] Cloning grc settings from Github"
 git clone https://github.com/garabik/grc.git
+
+echo "[+] Installing grc settings for current user"
 cd grc
 sudo ./install.sh
 rm -rf grc
+
+echo "[+] Copying ~/.grc/grc.bashrc to /etc/grc.bashrc"
+sudo cp ~/.grc/grc.bashrc /etc
+
+echo "[*] Complete. Try running: $ . ~/.bashrc"
