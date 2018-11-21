aa37638ea5afe81341ba786efff9c352970aeccf
Jesse Boyer <imboyus@gmail.com>
2018-11-21 09:19:49 -0500

    fix mmc

--- .prettier.config.js
+++ .prettier.config.js
@@ -1,12 +1,11 @@
-// @Not ready yet, looks good.
-// https://github.com/StarpTech/prettyhtml-vscode
 exports default = {
-  backetSpacing: true,
+  bracketSpacing: true,
   jsxBrackets: true,
-  arrowParens: "always",
+  arrowParens: 'always',
   //filepath: "src/index.js",
   //parser: "babylon(default)|flow|typescript|css|scss|less|json|...|vue|yaml",
-  trailingComma: "all",
+  parser: babylon,
+  trailingComma: 'all',
   tabs: false,
   tabWidth: 2,
   semi: true,
