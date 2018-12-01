16c9d72dce868e82298cd1a0f38c144ce4e1ff79
Jesse Boyer <imboyus@gmail.com>
2018-12-01 05:41:36 -0500

    Cleanup; Using PipEnv + PyEnv instead of venvwrapper

--- copy-cheat-cli.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo cp ./files/cht.sh /usr/local/bin
-echo "[+] Cht added to /usr/local/bin/cht.sh"
-echo "[+] Example Running: $ cht :list"
-echo ""
-echo "GitHub: https://github.com/chubin/cheat.sh"
-
