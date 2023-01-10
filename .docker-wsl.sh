9d282fb72e76d7cca5836956a21167ef4c592820
Jesse Boyer <imboyus@gmail.com>
2023-01-10 07:39:19 -0500

    upd

---
+++ .docker-wsl.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+DOCKER_DISTRO="Debian"
+DOCKER_DIR=/mnt/wsl/shared-docker
+DOCKER_SOCK="$DOCKER_DIR/docker.sock"
+export DOCKER_HOST="unix://$DOCKER_SOCK"
+if [ ! -S "$DOCKER_SOCK" ]; then
+    mkdir -pm o=,ug=rwx "$DOCKER_DIR"
+    chgrp docker "$DOCKER_DIR"
+    /mnt/c/Windows/System32/wsl.exe -d $DOCKER_DISTRO sh -c "nohup sudo -b dockerd < /dev/null > $DOCKER_DIR/dockerd.log 2>&1"
+fi
