44347a956bb739c33489a408e3ba200d47e5657b
Jesse Boyer <imboyus@gmail.com>
2023-06-01 14:24:03 -0400

    d

--- install-pnpm-node.sh
+++ install-pnpm-node.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 wget -qO- https://get.pnpm.io/install.sh | sh -
-cd ~
+cd ~ || exit
 pnpm env use --global lts
 pnpm add -g @pnpm/exe \
-  zx@latest \
   @builder.io/ai-shell \
-  pkg@latest \
-  hyperfine@latest \
-  pm2@latest \
-  vtop@latest \
-  esbuild@latest \
-  eslint@latest \
-  typescript@latest \
-  ts-node@latest \
-  turbo@latest \
-  fastify-cli@latest\
   @types/node\
   @types/tap\
-  tap
+  esbuild@latest \
+  eslint@latest \
+  fastify-cli@latest\
+  hyperfine@latest \
+  pkg@latest \
+  pm2@latest \
+  tap \
+  ts-node@latest \
+  turbo@latest \
+  typescript@latest \
+  vtop@latest \
+  zx@latest
 
 echo "[ Run ] $ ai config set OPENAI_KEY=keyhere"
 echo "[ Run ] $ ai config   (Change settings)"
